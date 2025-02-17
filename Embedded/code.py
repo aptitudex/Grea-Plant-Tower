8b7e80c8ba05b8e8eb0007732708ca1db605b35f
rhit-chews <chews@rose-hulman.edu>
2025-02-17 08:55:17 -0500

    updating code.py
    yay

--- Embedded/code.py
+++ Embedded/code.py
@@ -7,24 +7,32 @@ from neopixel import neopixel
 import time
 import math
 
+#######################################################################
+### change this value for each LED strip you are using on the tower ###
+num_led_strips = 4
+###                                                                 ###
+### change this value to match the amount of LEDs per strip         ###
+num_leds_per_strip = 30
+#######################################################################
+
 pixel_pin = board.GP28
-num_pixels = 120
+num_pixels = num_led_strips*num_leds_per_strip
 ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5, auto_write=False, pixel_order=ORDER)
-all_the_lights = [
- [[0, 1, 2, 3, 4, 5], [59, 58, 57, 56, 55, 54], [60, 61, 62, 63, 64, 65], [119, 118, 117, 116, 115, 114]],
- [[6, 7, 8, 9, 10, 11], [53, 52, 51, 50, 49, 48], [66, 67, 68, 69, 70, 71], [113, 112, 111, 110, 109, 108]],
- [[12, 13, 14, 15, 16, 17], [47, 46, 45, 44, 43, 42], [72, 73, 74, 75, 76, 77], [107, 106, 105, 104, 103, 102]],
- [[18, 19, 20, 21, 22, 23], [41, 40, 39, 38, 37, 36], [78, 79, 80, 81, 82, 83], [101, 100, 99, 98, 97, 96]],
- [[24, 25, 26, 27, 28, 29], [35, 34, 33, 32, 31, 30], [84, 85, 86, 87, 88, 89], [95, 94, 93, 92, 91, 90]]
-]
+all_the_lights = []
+for i in range(num_led_strips):
+    all_the_lights.append([])
+    for j in range(num_leds_per_strip):
+        if i%2 == 0:
+            all_the_lights[i].append(i*num_leds_per_strip+j)
+        if i%2 == 1:
+            all_the_lights[i].append((1+i)*num_leds_per_strip-j-1)
 statuses = [False, False]
 start_time = time.time()
 hour = 0
 count_for_lights = 0
 pixels.fill((50, 20, 50))
 pixels.show()
-count = 0
 
 i2c=busio.I2C(board.GP21, board.GP20)
 ads = ADS.ADS1115(i2c)
@@ -60,22 +68,22 @@ def handle_lights():
     pixels.fill((50, 20, 50))
     water_light = (20, 180, 255)
     tds_light = (255, 180, 20)
-    for i in range(5): # check each rowlayer
-        if statuses[0] & statuses[1]: # if both
-            for j in range(4):
-                if count_for_lights % 10 >= 5:
-                    pixels[all_the_lights[i][j][abs(4-(count_for_lights % 10))]] = tds_light
-                else:
-                    pixels[all_the_lights[i][j][abs(4-(count_for_lights % 10))]] = water_light
-        elif statuses[0]: # if only TDS
-            for j in range(4):
-                pixels[all_the_lights[i][j][count_for_lights%6]] = tds_light
-        elif statuses[1]: # if only water
-            for j in range(4):
-                pixels[all_the_lights[i][j][5-count_for_lights%6]] = water_light
+    for i in range(len(all_the_lights)): #for each led strip
+        for j in range(len(all_the_lights[0])): #for all lights in a strip
+            if statuses[0] & statuses[1]: # if both sensors are below threshold
+                if j % 6 == abs(4-(count_for_lights % 10)):
+                    if count_for_lights % 10 >= 5:
+                        pixels[all_the_lights[i][j]] = tds_light
+                    else:
+                        pixels[all_the_lights[i][j]] = water_light
+            elif statuses[0]: # if only TDs
+                if j % 6 == count_for_lights % 6:
+                    pixels[all_the_lights[i][j]] = tds_light
+            elif statuses[1]: # if only water
+                if j % 6 == 5 - count_for_lights % 6:
+                    pixels[all_the_lights[i][j]] = water_light
     count_for_lights += 1
-    if count_for_lights == 30:
-        count_for_lights = 0
+    count_for_lights %= 30
     pixels.show()
 
 while True :
